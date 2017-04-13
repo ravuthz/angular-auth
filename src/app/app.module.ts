dd134daba371bd04c65a48582f1ae1eb8f2a17df
Yo Vannaravuth <ravuthz@gmail.com>
2017-04-13 06:59:59 +0000

    Install Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,37 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngualarFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBpfxKFFpbh3g2E6OCSVdjw-fR-q9svPGk',
+  authDomain: '',
+  databaseURL: 'https://auth-899e9.firebaseio.com',
+  storageBucket: '',
+  messagingSenderId: '1055124975688'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngualarFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
